46b5bea2772484a2bc7ed2c4c423950807b6fffc
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-11 13:29:54 -0300

    Bump JARVIS to 1.2.10 for beta orb restore

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.2.9"
-BUILD = "2026.09.11-rotating-3d-orb.1"
+VERSION = "1.2.10"
+BUILD = "2026.09.11-beta-orb-restore.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
