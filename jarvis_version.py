e58517e64f1f81b7865aa7d10d205d938e1dd33e
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-11 17:20:14 -0300

    Bump JARVIS to 1.2.15 conversation redesign

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.2.14"
-BUILD = "2026.09.11-windows-mic-recovery.1"
+VERSION = "1.2.15"
+BUILD = "2026.09.11-conversation-ui-rebrand.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
