e795b6eb91372f9d7a90919a2f164a27e0fe37f5
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-11 12:16:43 -0300

    Bump JARVIS Desktop to 1.2.7

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.2.6"
-BUILD = "2026.09.11-conversation-sidebar.1"
+VERSION = "1.2.7"
+BUILD = "2026.09.11-conversation-sidebar-entrypoint.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
