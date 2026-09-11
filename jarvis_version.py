2e09e9e9d3118a465dc3cbf9b187a09227a0ec29
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-11 15:44:14 -0300

    Bump JARVIS to 1.2.12 for single-orb cinema subtitles

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.2.11"
-BUILD = "2026.09.11-orb-rebrand-audio-recovery.1"
+VERSION = "1.2.12"
+BUILD = "2026.09.11-single-orb-cinema-subtitles.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
