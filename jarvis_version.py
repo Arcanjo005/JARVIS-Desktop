46cc8ec6908049d90861fa60b590b630d0715c0b
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-14 08:27:58 -0300

    Bump JARVIS to 1.3.0 cinematic UI

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.2.15"
-BUILD = "2026.09.11-conversation-ui-rebrand.1"
+VERSION = "1.3.0"
+BUILD = "2026.09.14-cinematic-conversation-ui.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
