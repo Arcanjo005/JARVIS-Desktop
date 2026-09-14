58812829049390db6870efbd473500463ff3ae02
Arcanjo005 <arcanjo005lopes@gmail.com>
2026-09-14 09:36:22 -0300

    Version 1.3.2 photorealistic PBR visual

--- jarvis_version.py
+++ jarvis_version.py
@@ -1,8 +1,8 @@
 """Canonical public version identity for JARVIS Desktop."""
 from jarvis_identity import PUBLIC_NAME, WAKE_NAME, LEGACY_NAME
 
-VERSION = "1.3.1"
-BUILD = "2026.09.14-realistic-ui-voice-repair.1"
+VERSION = "1.3.2"
+BUILD = "2026.09.14-photorealistic-pbr-orb.1"
 CHANNEL = "stable"
 INTERNAL_NAME = "JARVIS"
 
